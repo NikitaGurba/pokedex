5ccfbf525d815c88afa31570b99a5ae60cd9730e
NikitaGurba <gurba.fortech@gmail.com>
2023-12-01 14:17:20 +0300

    dsaa

--- components/Chars.vue
+++ components/Chars.vue
@@ -7,59 +7,61 @@ const visibleDialog = ref(false);
 </script>
 
 <template>
-  <Dialog
-    v-model:visible="visibleDialog"
-    modal
-    :header="characteristics.abilities.name"
-    :style="{ width: '50rem' }"
-    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
-  >
-    <p>
-      {{ characteristics.abilities.description }}
-    </p>
-  </Dialog>
-  <Card
-    v-if="loaded"
-    class="w-fit"
-    :pt="{ content: 'p-0 flex flex-row gap-4 flex-wrap' }"
-  >
-    <template #content>
-      <div>
-        <b>Height</b>
-        <p>{{ characteristics.height }} m</p>
-      </div>
-      <div>
-        <b>Weight</b>
-        <p>{{ characteristics.weight }} kg</p>
-      </div>
-      <div>
-        <b>Gender</b>
-        <p>
-          <span v-for="gender in characteristics.gender" :key="gender">{{
-            gender
-          }}</span>
-        </p>
-      </div>
-      <div>
-        <b>Category</b>
-        <p>{{ characteristics.category }}</p>
-      </div>
-      <div
-        @click="visibleDialog = true"
-        class="cursor-pointer text-blue-500 dark:text-blue-300"
-      >
-        <b>Abilities</b>
-        <p>{{ characteristics.abilities.name }}</p>
-      </div>
-    </template>
-  </Card>
-  <Card
-    v-else
-    class="w-fit"
-    :pt="{ content: 'p-0 flex flex-row gap-4 flex-wrap' }"
-  >
-    <template #content>
-      <Skeleton height="1rem"></Skeleton>
-    </template>
-  </Card>
+  <div>
+    <Dialog
+      v-model:visible="visibleDialog"
+      modal
+      :header="characteristics.abilities.name"
+      :style="{ width: '50rem' }"
+      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+    >
+      <p>
+        {{ characteristics.abilities.description }}
+      </p>
+    </Dialog>
+    <Card
+      v-if="loaded"
+      class="w-fit"
+      :pt="{ content: 'p-0 flex flex-row gap-4 flex-wrap' }"
+    >
+      <template #content>
+        <div>
+          <b>Height</b>
+          <p>{{ characteristics.height }} m</p>
+        </div>
+        <div>
+          <b>Weight</b>
+          <p>{{ characteristics.weight }} kg</p>
+        </div>
+        <div>
+          <b>Gender</b>
+          <p>
+            <span v-for="gender in characteristics.gender" :key="gender">{{
+              gender
+            }}</span>
+          </p>
+        </div>
+        <div>
+          <b>Category</b>
+          <p>{{ characteristics.category }}</p>
+        </div>
+        <div
+          @click="visibleDialog = true"
+          class="cursor-pointer text-blue-500 dark:text-blue-300"
+        >
+          <b>Abilities</b>
+          <p>{{ characteristics.abilities.name }}</p>
+        </div>
+      </template>
+    </Card>
+    <Card
+      v-else
+      class="w-fit"
+      :pt="{ content: 'p-0 flex flex-row gap-4 flex-wrap' }"
+    >
+      <template #content>
+        <Skeleton height="1rem"></Skeleton>
+      </template>
+    </Card>
+  </div>
 </template>
